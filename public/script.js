e0983655f025964ebb33c4987052ba70c4c1fba6
Erin Peace <erinmpeace@gmail.com>
2018-05-07 19:26:18 +0100

    Changed site to be more 'content' focused

--- public/script.js
+++ public/script.js
@@ -15,7 +15,7 @@ function firstPopup() {
 function timedPopup() {
   setTimeout(function() {
     document.getElementById("modal-wrapper-two").style.display = "flex";
-  }, 3000);
+  }, 1000);
 }
 // This popup will appear after three seconds has elapsed. Try changing the number
 // from 3000 to any other number to see how that affects the timed popup.
@@ -32,6 +32,14 @@ function closePopup() {
 
 function closePopupTwo() {
   document.getElementById("modal-wrapper-two").style.display = "none";
+}
+
+function messagePopup() {
+  document.getElementById("messagePopup").style.display = "flex";
+}
+
+function closemessagePopup() {
+  document.getElementById("messagePopup").style.display = "none";
 }
 
 
@@ -65,137 +73,3 @@ function pad(val) {
     return valString;
   }
 }
-
-
-// POPUP Radio
-var modal = document.getElementById('ModalRadio');
-var btn = document.getElementById("notification-radio");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {modal.style.display = "block";}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// POPUP TV
-var modal = document.getElementById('ModalTV');
-var btn = document.getElementById("notification-tv");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {modal.style.display = "block";}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-
-// POPUP CEO
-var modal = document.getElementById('ModalCEO');
-var btn = document.getElementById("BtnCEO");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {modal.style.display = "block";}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// POPUP GIFs
-var modal = document.getElementById('ModalGIFs');
-var btn = document.getElementById("BtnGIFs");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
-
-//POPUP SUSHI
-var modal = document.getElementById('ModalSushi');
-var btn = document.getElementById("BtnSushi");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
-
-//POPUP STALKER
-var modal = document.getElementById('ModalStalker');
-var btn = document.getElementById("BtnStalker");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
-
-//POPUP KFC
-var modal = document.getElementById('ModalKFC');
-var btn = document.getElementById("BtnKFC");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
-
-//POPUP TODDLER
-var modal = document.getElementById('ModalToddler');
-var btn = document.getElementById("BtnToddler");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
-
-//POPUP InstaRobot
-var modal = document.getElementById('ModalInstaRobot');
-var btn = document.getElementById("BtnInstaRobot");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }}
