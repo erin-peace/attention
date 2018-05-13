6601f2284dfa4d675e4a2c8285b3faea21e0361e
Erin Peace <erinmpeace@gmail.com>
2018-05-13 17:05:59 +0100

    experimented with HTML quizzes

--- public/script.js
+++ public/script.js
@@ -1,3 +1,45 @@
+
+//Quiz
+
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
+
+
+	if (question1 == "Providence") {
+		correct++;
+}
+	if (question2 == "Hartford") {
+		correct++;
+}
+
+	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
+	var messages = ["Great job!", "That's just okay", "You really need to do better"];
+	var score;
+
+	if (correct == 0) {
+		score = 2;
+	}
+
+	if (correct > 0 && correct < 3) {
+		score = 1;
+	}
+
+	if (correct == 3) {
+		score = 0;
+	}
+
+	document.getElementById("after_submit").style.visibility = "visible";
+
+	document.getElementById("message").innerHTML = messages[score];
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("picture").src = pictures[score];
+	}
+
+
 //Timed Popups
 
 function timedPopup() {
@@ -30,12 +72,19 @@ function closePopupFour() {
   document.getElementById("RandomPopup1").style.display = "none";
 }
 
-
+//onClick Popups
 function MessagePopup() {
   document.getElementById("MessagePopup").style.display = "flex";
 }
 function closeMessagePopup() {
   document.getElementById("MessagePopup").style.display = "none";
+}
+
+function AttnQuizPopup() {
+  document.getElementById("AttnQuizPopup").style.display = "flex";
+}
+function closeAttnQuizPopup() {
+  document.getElementById("AttnQuizPopup").style.display = "none";
 }
 
 //Story Popups
