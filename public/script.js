e5bcc6d740435a8080d371d4207d74a22a8988cf
Erin Peace <erinmpeace@gmail.com>
2018-05-16 08:25:14 +0100

    made it work with the printer

--- public/script.js
+++ public/script.js
@@ -6,15 +6,17 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question3.value;
 	var correct = 0;
 
-
-	if (question1 == "Providence") {
-		correct++;
-}
-	if (question2 == "Hartford") {
-		correct++;
-}
+	if (question1 == "Selective") {
+	correct++;}
+	if (question2 == "Alternating") {
+	correct++;}
+	if (question3 == "Sustained") {
+	correct++;}
+	if (question4 == "Divided") {
+	correct++;}
 
 	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
 	var messages = ["Great job!", "That's just okay", "You really need to do better"];
@@ -33,7 +35,6 @@ function check(){
 	}
 
 	document.getElementById("after_submit").style.visibility = "visible";
-
 	document.getElementById("message").innerHTML = messages[score];
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 	document.getElementById("picture").src = pictures[score];
@@ -75,7 +76,7 @@ function closePopupFour() {
 function timedRandomPopup2() {
   setTimeout(function() {
     document.getElementById("RandomPopup2").style.display = "flex";
-  }, 1000);
+  }, 60000);
 }
 timedRandomPopup2();
 function closeRandomPopup2() {
@@ -139,3 +140,81 @@ function pad(val) {
     return valString;
   }
 }
+
+
+//SCROLL TO TopPicks
+(function(){
+	var backTop = document.getElementsByClassName('js-cd-top')[0],
+		// browser window scroll (in pixels) after which the "back to top" link is shown
+		offset = 2200,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offsetOpacity = 1200,
+		scrollDuration = 700
+		scrolling = false;
+	if( backTop ) {
+		//update back to top visibility on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkBackToTop, 250) : window.requestAnimationFrame(checkBackToTop);
+			}
+		});
+		//smooth scroll to top
+		backTop.addEventListener('click', function(event) {
+			event.preventDefault();
+			(!window.requestAnimationFrame) ? window.scrollTo(0, 0) : scrollTop(scrollDuration);
+		});
+	}
+
+	function checkBackToTop() {
+		var windowTop = window.scrollY || document.documentElement.scrollTop;
+		( windowTop > offset ) ? addClass(backTop, 'cd-top--show') : removeClass(backTop, 'cd-top--show', 'cd-top--fade-out');
+		( windowTop > offsetOpacity ) && addClass(backTop, 'cd-top--fade-out');
+		scrolling = false;
+	}
+
+	function scrollTop(duration) {
+	    var start = window.scrollY || document.documentElement.scrollTop,
+	        currentTime = null;
+
+	    var animateScroll = function(timestamp){
+	    	if (!currentTime) currentTime = timestamp;
+	        var progress = timestamp - currentTime;
+	        var val = Math.max(Math.easeInOutQuad(progress, start, -start, duration), 0);
+	        window.scrollTo(0, val);
+	        if(progress < duration) {
+	            window.requestAnimationFrame(animateScroll);
+	        }
+	    };
+
+	    window.requestAnimationFrame(animateScroll);
+	}
+
+	Math.easeInOutQuad = function (t, b, c, d) {
+ 		t /= d/2;
+		if (t < 1) return c/2*t*t + b;
+		t--;
+		return -c/2 * (t*(t-2) - 1) + b;
+	};
+
+	//class manipulations - needed if classList is not supported
+	function hasClass(el, className) {
+	  	if (el.classList) return el.classList.contains(className);
+	  	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+	}
+	function addClass(el, className) {
+		var classList = className.split(' ');
+	 	if (el.classList) el.classList.add(classList[0]);
+	 	else if (!hasClass(el, classList[0])) el.className += " " + classList[0];
+	 	if (classList.length > 1) addClass(el, classList.slice(1).join(' '));
+	}
+	function removeClass(el, className) {
+		var classList = className.split(' ');
+	  	if (el.classList) el.classList.remove(classList[0]);
+	  	else if(hasClass(el, classList[0])) {
+	  		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+	  		el.className=el.className.replace(reg, ' ');
+	  	}
+	  	if (classList.length > 1) removeClass(el, classList.slice(1).join(' '));
+	}
+})();
