94370647b2ca5daae5d9dcdd126574688d1fb2f1
Erin Peace <erinmpeace@gmail.com>
2018-05-19 23:19:15 +0100

    printer working

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var server = require('http').Server(app);
 
 var SerialPort = require('serialport');
 
-// var port = new SerialPort('/dev/cu.usbmodem1411', {
-//  baudRate: 9600
-//   });
+var port = new SerialPort('/dev/cu.usbmodem1411', {
+baudRate: 9600
+ });
 
 
 console.log('test');
